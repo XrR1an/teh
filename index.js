082261fceac5b442cbe7a0c9304b3b055b76ecb7
XrR1an <93810890+XrR1an@users.noreply.github.com>
2021-12-21 13:51:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require("axios")
-const urls = [""] //link project kamu
-const project = [""] //nama project kamu
+const urls = ["https://Bee4.muhamadwan.repl.co"] //link project kamu
+const project = ["Bee4"] //nama project kamu
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Berhasil Login Ke " + project + ", Time: "+ Date.now())).catch(() => {});
